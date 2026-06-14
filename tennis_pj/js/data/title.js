c96a9f37690ce8019737512b24459541b7ba2f24
naoki ishikawa <82689626+oneoknao@users.noreply.github.com>
2026-06-15 07:12:13 +0900

    Add files via upload

---
+++ tennis_pj/js/data/title.js
@@ -0,0 +1,13 @@
+// js/data/titles.js
+export const TITLES = [
+    { name: '魔王', min: 2000, color: '#ff00ff', condition: 'レート2000以上' },
+    { name: '卓神', min: 1800, color: '#ff4500', condition: 'レート1800-1999' },
+    { name: '卓聖', min: 1750, color: '#ff8c00', condition: 'レート1750-1799' },
+    { name: '卓帝', min: 1700, color: '#ffd700', condition: 'レート1700-1749' },
+    { name: '卓王', min: 1650, color: '#32cd32', condition: 'レート1650-1699' },
+    { name: '達人', min: 1600, color: '#00ced1', condition: 'レート1600-1649' },
+    { name: '師範', min: 1550, color: '#1e90ff', condition: 'レート1550-1599' },
+    { name: '猛者', min: 1500, color: '#4169e1', condition: 'レート1500-1549' },
+    { name: '初段', min: 1400, color: '#8a2be2', condition: 'レート1400-1499' },
+    { name: '見習い', min: 0, color: '#808080', condition: 'レート1400未満' }
+];
